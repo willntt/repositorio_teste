79b93b7393fa3444acf11aa4c677fefd193adcc2
willntt <william.yizima@global.ntt>
2021-11-30 15:21:09 -0300

    commit comendador module

---
+++ main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from module_test import imprime_algo
+
+print(imprime_algo())
---
+++ module_test/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*- 
+
+def imprime_algo():
+    return 'Olá comendador!'
+    
